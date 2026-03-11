ce977fa95f4e4f3e3c7ffaa0650d654fb2a1252d
ymanish1475-del <ymanish1475@gmail.com>
2026-03-11 21:13:53 +0530

    print in java

---
+++ JavaBasics/lec1.java
@@ -0,0 +1,10 @@
+// pakage.JavaBasics;
+
+public class lec1{
+    public static void main(String [] args){
+        System.out.println("hello world !!! ");
+          System.out.println(" this is manish");
+            System.out.println("i am the business person");
+        
+    }
+}
