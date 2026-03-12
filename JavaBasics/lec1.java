f4056352d90a1287da6623e4bd89320b55bd7d85
ymanish1475-del <ymanish1475@gmail.com>
2026-03-12 21:55:53 +0530

    backslash

--- JavaBasics/lec1.java
+++ JavaBasics/lec1.java
@@ -3,8 +3,8 @@
 public class lec1{
     public static void main(String [] args){
         System.out.println("hello world !!! ");
-          System.out.println(" this is manish");
-            System.out.println("i am the business person");
+          System.out.print(" this is manish\n");
+            System.out.print("i am the business person");
         
     }
 }
