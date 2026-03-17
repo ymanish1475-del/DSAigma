6d1a10f626fca422c133ddb14757e4062fbc2fb5
ymanish1475-del <ymanish1475@gmail.com>
2026-03-17 17:12:28 +0530

    vais variable in java

---
+++ JavaBasics/variable.java
@@ -0,0 +1,27 @@
+public class variable {
+
+    public static void main(String [] arg){
+
+        int a=10;
+        int b=20;
+        int sum=a+b;
+
+        String name = "manish yadav";
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+
+
+        System.out.println("sum="+sum);
+
+        System.out.println("name="+name);
+
+        sum=50;
+        System.out.println("sum="+sum);
+
+
+
+    }
+    
+}
