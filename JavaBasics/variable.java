2314b87fa3a5a00739531f55dbba7f3ec5a655ba
ymanish1475-del <ymanish1475@gmail.com>
2026-03-17 17:34:09 +0530

    primitive data type

--- JavaBasics/variable.java
+++ JavaBasics/variable.java
@@ -2,23 +2,42 @@ public class variable {
 
     public static void main(String [] arg){
 
-        int a=10;
-        int b=20;
-        int sum=a+b;
+        // int a=10;
+        // int b=20;
+        // int sum=a+b;
 
-        String name = "manish yadav";
+        // String name = "manish yadav";
 
-        System.out.println("a="+a);
+        // System.out.println("a="+a);
+        // System.out.println("b="+b);
+
+
+
+        // System.out.println("sum="+sum);
+
+        // System.out.println("name="+name);
+
+        // sum=50;
+        // System.out.println("sum="+sum);
+
+        byte b=8;
         System.out.println("b="+b);
 
+        char a = 's';
+        System.out.println("a="+a);
 
+        boolean m=true;
+        // boolean m=1;
+        System.out.println("m="+m);
 
-        System.out.println("sum="+sum);
+        // float frice = 10.6;
+        // System.out.println("f="+frice);
 
-        System.out.println("name="+name);
+        // long
+        // double
 
-        sum=50;
-        System.out.println("sum="+sum);
+        short n=240;
+        System.out.println("n="+n);
 
 
 
