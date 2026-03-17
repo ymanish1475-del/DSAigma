b2f915d8ea62674fc89734bf3916aa634bda0d64
ymanish1475-del <ymanish1475@gmail.com>
2026-03-17 19:41:21 +0530

    sum of a+b

---
+++ JavaBasics/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=37;
+        int b=890;
+        int sum=a+b;
+
+        System.out.println("sum="+sum);
+        // System.out.println("a+b="+a+b);
+    }
+    
+}
