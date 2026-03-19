20a38bf3c0c3ba6392f4a1cc0591b169cf9c4421
ymanish1475-del <ymanish1475@gmail.com>
2026-03-19 20:05:40 +0530

    sum with input

---
+++ JavaBasics/summ.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class summ {
+    public static void main (String [] arg){
+        Scanner sc  = new Scanner(System.in);
+        int a  = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+        int sum =a+b;
+
+        System.out.println("sum="+sum);
+
+
+
+
+
+
+    }
+}
