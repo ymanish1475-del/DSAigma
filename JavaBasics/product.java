151f326bfe78fd6c03a06885dd65ec07538f2a91
ymanish1475-del <ymanish1475@gmail.com>
2026-03-19 20:49:43 +0530

    product of a & b

---
+++ JavaBasics/product.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class product {
+
+     public static void main (String [] arg){
+        Scanner sc  = new Scanner(System.in);
+        int a  = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+        int mul =a*b;
+
+        System.out.println("mul of a & b ="+mul);
+
+     }
+    
+}
