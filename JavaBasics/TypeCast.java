f3d9ec9be6acbb768b1d972eac3e08831c3edb4a
ymanish1475-del <ymanish1475@gmail.com>
2026-03-23 20:10:19 +0530

    typecasting

--- JavaBasics/TypeCast.java
+++ JavaBasics/TypeCast.java
@@ -7,14 +7,24 @@ public class TypeCast {
 
         Scanner sc= new Scanner(System.in);
 
-        float a = sc.nextInt();
-        System.out.println(a);
+        // float a = sc.nextInt();
+        // System.out.println(a);
 
         // int a= sc.nextInt();
 
         // int b= a;
 
         // System.out.println("b="+b);
+
+        // float a = 999.9979f;
+        // int b= (int)a;
+
+        // System.out.println(b);
+
+        char a ='z';
+
+        int b= a;
+        System.out.println(b);
     }
     
 }
