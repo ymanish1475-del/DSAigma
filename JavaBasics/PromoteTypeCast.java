73164874eebbc817e0473f57644fea60c68ca2a5
ymanish1475-del <ymanish1475@gmail.com>
2026-03-26 13:32:44 +0530

    promated typecast

--- JavaBasics/PromoteTypeCast.java
+++ JavaBasics/PromoteTypeCast.java
@@ -4,14 +4,24 @@ public class PromoteTypeCast {
     public static void main(String [] args){
 
 
-        int a=26;
-        float b= (float)67.09;
-        long c = 786;
-        double d = 67;
+        // int a=26;
+        // float b= (float)67.09;
+        // long c = 786;
+        // double d = 67;
 
-        double  e = a+b-c*d;
+        // double  e = a+b-c*d;
 
-        System.out.println("e="+e);
+        // System.out.println("e="+e);
+
+        // byte b = 8;
+        // b= 2*b;
+        byte b=8;
+        b = (byte)(2*b);
+        System.out.println("b="+b);
+
+        int a= 5;
+        a= a*a;
+        System.out.println("a="+a);
 
 
 
