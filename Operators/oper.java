34bbed3dbb411f1de662d6fab9afb4e713211412
ymanish1475-del <ymanish1475@gmail.com>
2026-03-26 22:05:12 +0530

    operator

---
+++ Operators/oper.java
@@ -0,0 +1,84 @@
+
+
+// package Operators;
+import java.util.*;
+
+
+public class oper {
+
+    public static void main (String [] args){
+
+        Scanner s = new Scanner(System.in);
+
+        // ouput 55555
+
+        // System.out.print("a=");
+        // int a = s.nextInt();
+
+        // System.out.println("a="+a);
+
+        // int b =a++ ;
+
+        // System.out.println(b);
+
+        // int c =b++ ;
+
+        // System.out.println(c);
+
+        // int d =c++ ;
+
+        // System.out.println(d);
+
+        // output 5678
+
+        System.out.print("a=");
+        int a = s.nextInt();
+
+        System.out.println("a="+a);
+
+        int b =++a ;
+
+        System.out.println("b="+b);
+
+        int c =++b;
+
+        System.out.println("c="+c);
+
+        int d =++c;
+
+        System.out.println("d="+d);
+
+
+
+        
+
+        
+
+        // System.out.print("a=");
+        // int a = sc.nextInt();
+
+        
+        // System.out.print("b=");
+        // int b= sc.nextInt();
+
+        // int sum = a+b;
+        // int dif= a-b;
+        // int mul= a*b;
+        // float div= (float)a/b;
+
+        // int mod = a%b;
+
+        // System.out.println("sum="+sum);
+        // System.out.println("dif="+dif);
+        // System.out.println("mul="+mul);
+        // System.out.println("div="+div);
+        // System.out.println("mod="+mod);
+
+
+
+
+
+
+    }
+    
+}
