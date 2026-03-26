d95b9f78337e9343fb665d5142a477a5c8c56c60
ymanish1475-del <ymanish1475@gmail.com>
2026-03-26 12:22:12 +0530

    prome type cast

---
+++ JavaBasics/PromoteTypeCast.java
@@ -0,0 +1,21 @@
+public class PromoteTypeCast {
+
+
+    public static void main(String [] args){
+
+
+        int a=26;
+        float b= (float)67.09;
+        long c = 786;
+        double d = 67;
+
+        double  e = a+b-c*d;
+
+        System.out.println("e="+e);
+
+
+
+
+
+    }
+}
