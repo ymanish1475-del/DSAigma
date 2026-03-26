a55cf8c62cdcce2a58dcc904b851da06741c2bf6
ymanish1475-del <ymanish1475@gmail.com>
2026-03-26 13:51:05 +0530

    avearage in java

---
+++ JavaBasics/Question.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class Question {
+
+    public static void main (String [] args){
+
+        Scanner sc= new Scanner(System.in);
+        // a
+        System.out.print("a=");
+        int a = sc.nextInt();
+
+        // b
+
+        System.out.print("b=");
+        int b = sc.nextInt();
+
+        // c
+
+        System.out.print("c=");
+        int c = sc.nextInt();
+
+        float av = (a+b+c)/3f;
+
+        System.out.println("av="+av);
+
+
+
+
+    }
+    
+}
