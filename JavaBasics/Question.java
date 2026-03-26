c00515d1d4f6e73580080a24f65d50e2495296e0
ymanish1475-del <ymanish1475@gmail.com>
2026-03-26 14:21:49 +0530

    making bill

--- JavaBasics/Question.java
+++ JavaBasics/Question.java
@@ -7,18 +7,51 @@ public class Question {
 
         Scanner sc= new Scanner(System.in);
 
-        System.out.print("give the side of squre=");
-        int a = sc.nextInt();
+        System.out.println("your bill");
 
-        System.out.println("your input="+a);
+        System.out.print("pencil=");
+        float p = sc.nextFloat();
 
-        int sq = a*a;
+        System.out.print("pen=");
+        float n = sc.nextFloat();
+
+        System.out.print("eraser=");
+        float e = sc.nextFloat();
+
+        System.out.println("your products");
+
+        System.out.println("pencil="+p);
+        System.out.println("pen="+n);
+        System.out.println("eraser="+e);
+
+        float t = (float)p+n+e ;
+
+        System.out.println("total cost ="+t);
+
+        // adding gst
+
+        float gst = (float)t+t*(18f/100f);
+
+        System.out.println(" with gst = "+gst);
+
+    
+
 
         
 
-        System.out.println("area of squre="+sq+"m^2");
+// question 2
+        // System.out.print("give the side of squre=");
+        // int a = sc.nextInt();
 
+        // System.out.println("your input="+a);
 
+        // int sq = a*a;
+
+        
+
+        // System.out.println("area of squre="+sq+"m^2");
+
+// question 1
         // a
         // System.out.print("a=");
         // int a = sc.nextInt();
