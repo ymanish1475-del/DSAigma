9ba6adf80f5acc6e638b992bd7752480ed08ea0e
ymanish1475-del <ymanish1475@gmail.com>
2026-03-27 20:21:10 +0530

    assop

---
+++ Operators/AssOp.java
@@ -0,0 +1,11 @@
+public class AssOp {
+
+    public static void main(String [] args){
+
+        int A = 10;
+        // A= A+10;
+        A+=10;
+        System.out.println("a="+A);
+    }
+    
+}
