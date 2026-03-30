960db00a8b05af9f02353b8610a31d66fcdb0c8b
ymanish1475-del <ymanish1475@gmail.com>
2026-03-30 14:21:44 +0530

    op

--- Operators/AssOp.java
+++ Operators/AssOp.java
@@ -2,17 +2,20 @@ public class AssOp {
 
     public static void main(String [] args){
 
-        int A = 10;
-        // A= A+10;
-        A+=10;
-        System.out.println("a="+A);
+        // int A = 10;
+        // // A= A+10;
+        // A+=10;
+        // System.out.println("a="+A);
 
-        int B = 10;
+        // int B = 10;
         
-        // B=B-10;
-        B-=10;
-        // B+=10;
-        System.out.println("b="+B);
+        // // B=B-10;
+        // // B-=10;
+        // // B*=5;
+        // B%=3;
+        // // B+=10;
+        // System.out.println("b="+B);
+        int x = 10, y = 5;int exp1 = (y * (x / y + x / y));int exp2 = (y * x / y + y * x / y);System.out.println(exp1);System.out.println(exp2);
     }
     
 }
