3426787921b9c18e40ddc3756814868567a2c830
ymanish1475-del <ymanish1475@gmail.com>
2026-04-01 21:22:36 +0530

    swith case statement

---
+++ ConditionalStatement/SwitchSt.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SwitchSt {
+
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+        int numm = sc.nextInt();
+
+        int num = 2;
+
+        switch (numm) {
+            case 1: System.out.println("Faaaa");
+                
+                break;
+            case 2: System.out.println("han");
+                
+                break;
+        
+            default: System.out.println("faaa");
+                break;
+        }
+    }
+    
+}
