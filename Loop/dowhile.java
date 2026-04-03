2dd1a875ac4e27f824795e4bea8367f00579b035
ymanish1475-del <ymanish1475@gmail.com>
2026-04-03 15:16:07 +0530

    do whle

---
+++ Loop/dowhile.java
@@ -0,0 +1,16 @@
+// package Loop;
+
+public class dowhile {
+
+    public static void main(String [] args){
+
+        int n=0;
+
+        System.out.print("f");
+        do{
+            System.out.print("a");
+            n++;
+        }while(n<10);
+    }
+    
+}
