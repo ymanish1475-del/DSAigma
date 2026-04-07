f7e481367f8a010eac3ba0d5493597b2ae567812
ymanish1475-del <ymanish1475@gmail.com>
2026-04-07 21:26:53 +0530

    FACTORIAL

---
+++ Loop/factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String [] args ){
+
+        Scanner sc= new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int fact=1;
+
+        for(int i=1;i<=n;i++){
+            fact=i*fact;
+        }
+        System.out.println(fact);
+
+
+    }
+    
+}
