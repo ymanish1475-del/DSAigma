2ba228e21d03023f19b1ebcdf744f70f48621e33
ymanish1475-del <ymanish1475@gmail.com>
2026-04-09 21:27:08 +0530

    tringle in star

---
+++ Pattern/triangle.java
@@ -0,0 +1,23 @@
+// package Pattern;
+
+import java.util.Scanner;
+
+public class triangle {
+
+    public static void main(String[] args) {
+        int m=4 ;
+        // int n=4;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n=");
+        int n=sc.nextInt();
+        
+        for(int i=0;i<n;i++){
+            // System.out.println("*");
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
