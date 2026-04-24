7df827ebed9325993fc9bd598bdf06bea19ba47b
ymanish1475-del <ymanish1475@gmail.com>
2026-04-24 15:06:22 +0530

    hello world with function

---
+++ FunctionAndMethod/helloWorld.java
@@ -0,0 +1,16 @@
+// package FunctionAndMethod;
+
+public class helloWorld {
+
+    public static void hello(){
+        System.out.println("hello world");
+        return;
+        // if function type is void only then "return " does  not to requre to write in function otherwise its compulsory or through the error
+    }
+
+    public static void main(String[] args) {
+        hello();
+        
+    }
+    
+}
