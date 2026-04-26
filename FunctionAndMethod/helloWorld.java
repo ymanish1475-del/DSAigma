010d4ffd952620cf7e5af749a4a435e522c7ff53
ymanish1475-del <ymanish1475@gmail.com>
2026-04-26 22:39:07 +0530

    parameter  and argument

--- FunctionAndMethod/helloWorld.java
+++ FunctionAndMethod/helloWorld.java
@@ -9,7 +9,7 @@ public class helloWorld {
         return;
         // if function type is void only then "return " does  not to requre to write in function otherwise its compulsory or through the error
     }
-    public static int calsum(int n, int m) {
+    public static int calsum(int n, int m) {    //parametre or formal parameter
         // Scanner sc = new Scanner(System.in);
         // int a = sc.nextInt();
         // int b= sc.nextInt();
@@ -26,7 +26,7 @@ public class helloWorld {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b= sc.nextInt();
-        int sum =calsum(a,b);
+        int sum =calsum(a,b);   //argument 
         System.out.println("sum is : "+sum);
         // int sum = a+b;
         // System.out.println(sum);
