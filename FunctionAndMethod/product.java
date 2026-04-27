3915ce81d71ebdf4dd2af46187e248e2072e8ae2
ymanish1475-del <ymanish1475@gmail.com>
2026-04-27 18:54:41 +0530

    product though function

---
+++ FunctionAndMethod/product.java
@@ -0,0 +1,21 @@
+public class product {
+
+    static int product(int a,int b){
+        int mul=a*b;
+        // System.out.println("product= "+mul);
+        return mul;
+    }
+
+    public static void main(String[] args) {
+
+        int a= 67;
+        int b=78;
+        // System.out.println(product(a, b));
+        int produc=product(a, b);
+        System.out.println("product= "+produc);
+        
+
+        
+    }
+    
+}
