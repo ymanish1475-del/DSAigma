50cdcc849ed0846c59a2123becc64a0ccaf6caf6
ymanish1475-del <ymanish1475@gmail.com>
2026-05-04 20:36:17 +0530

    solve binomial coeficient

--- FunctionAndMethod/binomialCoefficient.java
+++ FunctionAndMethod/binomialCoefficient.java
@@ -1,6 +1,47 @@
 public class binomialCoefficient {
 
+    static int factorial( int n){
+        int fact=1;
+        //  if(n==1) return 1;
+
+
+
+        
+            for(int i=1;i<=n;i++){
+             fact= fact*i;
+        
+        // System.out.println("fact= "+fact);
+
+        }
+        return fact;
+    }
+
+    static int binCoefficient(int n,int r){
+
+        int fact_n= factorial(n);
+        int fact_r= factorial(r);
+        int fact_nmr= factorial(n-r);
+
+        int binCoeff= fact_n/(fact_r*fact_nmr);
+
+        return binCoeff;
+
+        // int fact=1;
+
+        // for(int i=1;i<=n;i++){
+        //     fact=fact*i;
+        // }
+        // return fact;
+
+    }
+
     public static void main(String[] args) {
+
+        int n=10;
+        int r=4;
+
+
+        System.out.println(binCoefficient(n, r));
         
     }
     
