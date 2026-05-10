1da49fcdd538dbed4d4579291d0132cd63b5c92b
ymanish1475-del <ymanish1475@gmail.com>
2026-05-10 15:02:18 +0530

    FUNCTION OVERLOADING

---
+++ FunctionAndMethod/calC2.java
@@ -0,0 +1,21 @@
+public class calC2 {
+
+     static int sum(int a,int b) {
+
+        return a+b;
+        
+    }
+
+     static float sum( float a,float  b) {
+
+        return a+b;
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(7,4));
+        System.out.println(sum(7.65f,4.87f));
+        
+    }
+    
+}
