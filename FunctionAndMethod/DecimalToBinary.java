c8fe6471406d87e6333ee379b26bb65d86bac502
ymanish1475-del <ymanish1475@gmail.com>
2026-05-20 20:51:19 +0530

    decimal to binary

--- FunctionAndMethod/DecimalToBinary.java
+++ FunctionAndMethod/DecimalToBinary.java
@@ -1,22 +1,29 @@
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
     public static void deciTobinary(int deci){
         int bin=0;
+        int value=0;
 
         while(deci>0){
             bin=deci%2;
             deci=deci/2;
-            bin=(bin*10)+bin;
+            value=(value*10)+bin;
 
         }
-        System.out.println(bin);
+        System.out.println(value);
     }
 
     public static void main(String[] args) {
         
-        int deci=211;
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter num : ");
+        int deci=sc.nextInt();
         deciTobinary(deci);
 
     }
     
+
 }
