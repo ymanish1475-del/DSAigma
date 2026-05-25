c1ee318b80a8638fae0f4c4b722cd147e53f37f6
ymanish1475-del <ymanish1475@gmail.com>
2026-05-25 20:34:38 +0530

    InvHalfPyraNum

--- Pattern-Advanced/InvHalfPyraNum.java
+++ Pattern-Advanced/InvHalfPyraNum.java
@@ -1,7 +1,24 @@
 // package Pattern-Advanced;
 
 public class InvHalfPyraNum {
+
+    public static void pyramid(int n){
+        int m=n;
+
+        for(int j=n;j>=1;j--){
+            for(int i=1;i<=n;i++){
+            System.out.print(i);
+            
+          }
+          System.out.println();
+          n=n-1;
+        }
+        
+
+    }
     public static void main(String[] args) {
         
+        int n=5;
+        pyramid(n);
     }
 }
