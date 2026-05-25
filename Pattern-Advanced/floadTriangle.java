7a45abd34e6bb54d278e50c70b01879bcf004b96
ymanish1475-del <ymanish1475@gmail.com>
2026-05-25 21:30:08 +0530

    floadTriangle

---
+++ Pattern-Advanced/floadTriangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class floadTriangle {
+
+    public static void pyramid( int n) {
+        int m=1;
+        while(m<n){
+            for(int i=1;i<=n;i++){
+
+                for(int j=1;j<=i;j++){
+                    System.out.print(m+" ");
+                    m++;
+                }
+                System.out.println();
+                
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER HOW MUCH LINE YOU WANT PRINT FLOAD TRIANGLE : ");
+        int n=sc.nextInt();
+        pyramid(n);
+
+    }
+}
