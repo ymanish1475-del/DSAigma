6e2f9a906fa264f1464c1635c545cfb31af187ee
ymanish1475-del <ymanish1475@gmail.com>
2026-05-26 23:26:11 +0530

    butterFly

---
+++ Pattern-Advanced/butterFly.java
@@ -0,0 +1,16 @@
+public class butterFly {
+
+    public static void butterfly(int n) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        butterfly(n);
+    }
+}
