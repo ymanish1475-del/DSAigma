6e7d1813cd7c73461573943b53c23b151ce82a0c
ymanish1475-del <ymanish1475@gmail.com>
2026-05-26 23:06:59 +0530

    trinagle

--- Pattern-Advanced/Trinagle.java
+++ Pattern-Advanced/Trinagle.java
@@ -3,10 +3,10 @@ public class Trinagle {
     public static void triangle(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                if((i+1)%(j)==0 ){
-                    System.out.print("0");
-                }else{
+                if((i+j)%2==0 ){
                     System.out.print("1");
+                }else{
+                    System.out.print("0");
                 }
             }
             System.out.println();
