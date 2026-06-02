0dbab3c018d1ed669a0c3f291bca425c47f2a320
ymanish1475-del <ymanish1475@gmail.com>
2026-06-02 20:08:59 +0530

    hollow Rhombus

---
+++ Pattern-Advanced/hollowRhombus.java
@@ -0,0 +1,25 @@
+public class hollowRhombus {
+
+    public static void hollowRhombus(int n) {
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            
+            for(int j=1;j<=n;j++){
+                if((i==1||j==n)||(i==n||j==1)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        
+        int n=5;
+        hollowRhombus(n);
+    }
+}
--- Pattern-Advanced/rhombus.java
+++ Pattern-Advanced/rhombus.java
@@ -3,9 +3,12 @@ public class rhombus {
     public static void rhombus(int n) {
         
         for(int i=1;i<=n;i++){
+
+            //spaces
             for(int j=1;j<=n-i ; j++){
                 System.out.print(" ");
             }
+            //star
             for(int j=1;j<=n;j++){
                 System.out.print("*");
             }
