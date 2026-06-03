951a33c3752d779ac6b49c5deb12065cbb6b00f0
ymanish1475-del <ymanish1475@gmail.com>
2026-06-03 21:10:23 +0530

    array

---
+++ Array/array.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        
+        int arr[]=new int [10];
+        Scanner sc= new Scanner(System.in);
+        System.out.println(arr.length);
+        int phy;
+        // phy=
+        // arr[0]=sc.nextInt();
+        // arr[1]=sc.nextInt();
+        // arr[2]=sc.nextInt();
+        // arr[3]=sc.nextInt();
+
+        // System.out.println("physics = "+arr[0]);
+        // System.out.println("math = "+arr[1]);
+        // System.out.println("computer = "+arr[2]);
+        // System.out.println("coding = "+arr[3]);
+
+        // float ave= (arr[0]+arr[1]+arr[2]+arr[3])/4;
+        // System.out.println(ave);
+        int m[]=[78,89,56,45]
+        System.out.println(m);
+
+        // arr[1]=97;
+        // System.out.println(arr[1]);
+
+        
+    }
+}
