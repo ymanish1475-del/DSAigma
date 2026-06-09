1261c5409edb86be8ae5f0876635366e434a2238
ymanish1475-del <ymanish1475@gmail.com>
2026-06-09 20:36:00 +0530

    BINARY SEARCH

---
+++ Array/binarySearch.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class binarySearch {
+
+    public static int  binarySearch(int num[],int key) {
+
+        int start=0 , end=num.length-1;
+        int mid=0;
+        while(start<=end){
+             mid = (start+end)/2;
+
+            if(num[mid]==key){
+                // System.out.println("found in "+mid);
+                return mid;
+            }else if(num[mid]>key){
+                end=mid-1;
+            }else {
+                start=mid+1;
+            }
+           
+
+
+        }
+         return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("SEARCH NUM B/W 1 - 58 ");
+
+        int num[]={1,2,3,4,5,6,7,8,9,10,11,12,13,24,36,47,58};
+        int key=sc.nextInt();
+        // int ans=binarySearch(num, key);
+        // System.out.println(ans);
+        System.out.println(binarySearch(num, key));
+    }
+}
