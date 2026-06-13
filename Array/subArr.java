c01a6cf6e2c832c995646242c3deab7096bb59cd
ymanish1475-del <ymanish1475@gmail.com>
2026-06-13 10:34:45 +0530

    subArr

--- Array/subArr.java
+++ Array/subArr.java
@@ -1,27 +1,35 @@
+// import javax.sound.sampled.SourceDataLine;
+
 public class subArr {
     public static void subArr(int nums[]) {
         int len=nums.length;
-        int first = 0;
-        int end=nums.length-1;
+        // int first = 0;
+        // int end=1;
+        int ts=0;
+        // int sum
 
-        for(int i=first;i<len;i++){
-            for(int j=;j<=end;j++){
-                System.out.print(nums[first]+" "+nums[j]);
+        for(int i=0;i<len;i++){
+            // System.out.print(nums[i]+" ");
+            for(int j=i;j<len;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    System.out.print("["+nums[k]+"],"+" ");
+                    sum=sum+nums[k];
+                    System.out.print(sum);
+                }
+                ts++;
+
+                
+
+                System.out.println();
+                
+            
 
             }
-            first++;
-            end--;
             System.out.println();
-
         }
-        // for(int i=0;i<len;i++){
-        //     System.out.print(nums[i]+" ");
-        //     for(int j=i+1;j<l;j++){
-        //         System.out.print(nums[i]);
-        //         System.out.print(nums[j]+" ");
-        //     }
-        //     System.out.println();
-        // }
+        System.out.println("total subarr="+ts);
+       
         
     }
     public static void main(String[] args) {
