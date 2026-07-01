2fa3fbc3209bb3c01c83d15247fd57dfeb3d0b16
ymanish1475-del <ymanish1475@gmail.com>
2026-07-01 16:39:11 +0530

    uniqArr

--- ArraysMarvel/Ass.java
+++ ArraysMarvel/Ass.java
@@ -1,3 +1,27 @@
 public class Ass {
+
+    //find array have uniq num
+
+    public static boolean uniq(int num[] ) {
+        int len= num.length;
+
+       for(int i=0;i<len;i++){
+        for(int j=i+1;j<len ;j++){
+            if(num[i]==num[j]){
+                return false;
+            }
+        }
+       }
+
+        return true;
+        
+    }
+    public static void main(String[] args) {
+
+        int num[]={1,2,3,4,4};
+       
+        System.out.println( uniq(num));
+        
+    }
     
 }
