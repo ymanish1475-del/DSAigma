b34f37aa4995c7e64b1be86551ac1dc290ad1005
ymanish1475-del <ymanish1475@gmail.com>
2026-07-01 16:15:02 +0530

    BuySellStock

--- ArraysMarvel/BuySellStock.java
+++ ArraysMarvel/BuySellStock.java
@@ -1,3 +1,41 @@
 public class BuySellStock {
+
+    public static void BuySellStock(int [] arr) {
+
+        int maxProfit= 0;
+        int buyStock=Integer.MAX_VALUE;
+        int sellStock=0;
+
+        int len= arr.length;
+
+        for(int i=0;i<len;i++){
+            int profit=0;
+
+            if(buyStock<arr[i]){
+                profit=arr[i]-buyStock;
+
+                if(profit>maxProfit){
+                    maxProfit=profit;
+                }
+            }
+            else{
+                buyStock=arr[i];
+            }
+           
+        }
+        System.out.println();
+        
     
+        System.out.println(maxProfit);
+
+        
+    }
+
+
+    public static void main(String[] args) {
+
+        int arr[]={7,1,5,3,6,41};
+        BuySellStock(arr);
+        
+    }
 }
